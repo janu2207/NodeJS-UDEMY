df2f917f684f07f02e1fd808f38393a671d7be95
Jahnavi Bantupalli <jbantupa@asu.edu>
2019-02-09 17:45:37 -0700

    Fixing bugs before

--- app.js
+++ app.js
@@ -3,21 +3,28 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const app = express();
+const MONGODB_URI='mongodb+srv://janu2207:Jalend12@cluster0-sgdxc.mongodb.net/shop?retryWrites=true';
 
+const app = express();
+const store = new MongoDBStore({
+  uri:MONGODB_URI,
+  collection:'sessions'
+});
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
+const authRoutes= require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({secret:'my secret',resave : false, saveUninitialized:false,store:store}))
 app.use((req, res, next) => {
   User.findById('5c5b92578cfd96adf06f7ac5')
     .then(user => {
@@ -29,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
---
+++ controllers/auth.js
@@ -0,0 +1,21 @@
+exports.getLogin =(req,res,next)=>{
+//const isLoggedIn= req.get('Cookie').split('=')[1];
+   console.log(req.session)
+    res.render('auth/login',{
+        path:'/login',
+        pageTitle:'Login',
+        isAuthenticated : false
+    })
+};
+
+exports.postLogin =(req,res,next)=>{
+    req.session.isLoggedIn = true;
+    res.redirect('/')
+};
+
+exports.postLogout =(req,res,next)=>{
+    req.session.destroy((err)=>{
+        console.log(err);
+        res.redirect('/');
+    });
+};
